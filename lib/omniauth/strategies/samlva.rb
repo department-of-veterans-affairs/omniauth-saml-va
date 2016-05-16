29b9786b5af8e5aa29c167db183d2ececd412d95
Paul Tagliamonte <paul.tagliamonte@va.gov>
2016-05-16 13:47:37 -0400

    Add in logic to short-circut request phase if IAM

--- lib/omniauth/strategies/samlva.rb
+++ lib/omniauth/strategies/samlva.rb
@@ -7,6 +7,8 @@ require 'base64'
 module OmniAuth
   module Strategies
     class SAMLVA < OmniAuth::Strategies::SAML
+      attr_accessor :iam_provider
+
       def initialize(app, issuer=nil, private_key=nil, certificate=nil, configuration_xml=nil, sign=false, options={}, &block)
         doc = Nokogiri.XML(File.open(configuration_xml, 'rb'))
         cert = OpenSSL::X509::Certificate.new(Base64.decode64(doc.xpath(
@@ -17,6 +19,8 @@ module OmniAuth
         private_key = File.read(private_key)
         certificate = File.read(certificate)
 
+        self.iam_provider = options[:va_iam_provider]
+
         options[:issuer] = issuer
         options[:private_key] = private_key
         options[:certificate] = certificate
@@ -24,6 +28,8 @@ module OmniAuth
         options[:idp_sso_target_url] ||= location
         options[:idp_cert] ||= cert.to_pem
         options[:name_identifier_format] ||= "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+        options[:allowed_clock_drift] ||= 30.seconds
+
         options[:security] = {
           :authn_requests_signed    => sign,
           :logout_requests_signed   => sign,
@@ -38,9 +44,10 @@ module OmniAuth
         super(app, options, &block)
       end
 
-      # def request_phase
-      #   redirect("#{options[:idp_sso_target_url]}?SPID=#{options[:issuer]}")
-      # end
+      def request_phase
+        return redirect("#{options[:idp_sso_target_url]}?SPID=#{options[:issuer]}") if self.iam_provider == :iam
+        super
+      end
     end
   end
 end
